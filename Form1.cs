f4b6885d616ca0514d26873a8bc6c64b58dfb866
IvanSharpov <Ivansharpov@ya.ru>
2015-07-07 14:44:42 +0400

    first commit

---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int xz = 0;
+        int otvet;
+        string ifer;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            xz = 1;
+            ifer = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+          
+            textBox1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+           
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+            if (xz == 1)
+            {
+                int otvet = Convert.ToInt32(ifer) + Convert.ToInt32(textBox1.Text);
+                textBox1.Text = Convert.ToString(otvet);
+                xz = 0;
+            }
+            else if (xz == 2)
+            {
+                int otvet = Convert.ToInt32(ifer) - Convert.ToInt32(textBox1.Text);
+                textBox1.Text = Convert.ToString(otvet);
+                xz = 0;
+            }
+            else if (xz == 3)
+            {
+                int otvet = Convert.ToInt32(ifer) * Convert.ToInt32(textBox1.Text);
+                textBox1.Text = Convert.ToString(otvet);
+                xz = 0;
+            }
+            else if (xz == 4)
+            {
+                int otvet = Convert.ToInt32(ifer) / Convert.ToInt32(textBox1.Text);
+                textBox1.Text = Convert.ToString(otvet);
+                xz = 0;
+            }
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            xz = 2;
+            ifer = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            xz = 3;
+            ifer = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            xz = 4;
+            ifer = textBox1.Text;
+            textBox1.Text = "";
+        }
+         
+    }
+}
